e36f7e05cf181fe0e987f9cf787945ba27673a4c
Emanuel Friz <emanuelfriz@gmail.com>
2014-04-04 10:37:30 -0400

    Linea Agregada por Web

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -3,7 +3,9 @@ package cl.taller.git;
 public class Main {
 
 	public static void main(String[] args) {
-	 System.out.println("Segundo Commit");	
+	 System.out.println("Segundo Commit");
+	 System.out.println("Linea Agregada por Web");
+	 
 	}
 
 }
