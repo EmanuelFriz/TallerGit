43487909d482682489c76b7a4613a5eb8d80ce13
Emanuel Friz <emanuelfriz@gmail.com>
2014-04-04 11:17:33 -0400

    modificacion a "Linea Agregada  Por Web"

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	 System.out.println("Segundo Commit");
-	 System.out.println("Linea Agregada por Web");
+	 System.out.println("Linea Agregada");
 	 System.out.println("Modificando el fichero main.java en github.com");
 	 
 	}
